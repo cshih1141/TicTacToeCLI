8fa7a2e93cc7dab482ad7d62a8fd01433b1817cc
Craig Shih <cshih1141@gmail.com>
2019-11-25 15:13:21 -0800

    completed tictactoeCLI

--- TicTacToe.js
+++ TicTacToe.js
@@ -20,9 +20,56 @@ const readline = require('readline').createInterface({
 })
 
 
-const checkWinConditions = () => {
+const checkRowConditions = (player) => {
+  for (let i = 0; i < gameDecisions.length; i++) {
+    let count = 0;
+    for (let j = 0; j < gameDecisions[i].length; j++) {
+      if(gameDecisions[i][j] === player) {
+        count++;
+      }
+    }
+    if(count === 3) {
+      return true;
+    }
+  }
+  return false;
+}
 
+const checkColConditions = (player) => {
+  for (let i = 0; i < gameDecisions.length; i++) {
+    let count = 0;
+    for (let j = 0; j < gameDecisions[i].length; j++) {
+      if(gameDecisions[j][i] === player) {
+        count++;
+      }
+    }
+    if(count === 3) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const checkDiagonalConditions = (player) => {
+  if ((gameDecisions[0][0] === player && gameDecisions[1][1] === player && gameDecisions[2][2] === player) 
+      || (gameDecisions[0][2] === player && gameDecisions[1][1] === player && gameDecisions[2][0] === player)) {
+        return true;
+      }
+  return false;
+}
+
+const checkWinConditions = (player) => {
+  if (checkRowConditions(player) || checkColConditions(player) || checkDiagonalConditions(player)) {
+    return false;
+  }
   return true;
+}
+
+const validMove = (row, col) => {
+  if(gameDecisions[row][col] === ' ') {
+    return true;
+  }
+  return false;
 }
 
 const enterPlayerName = () => {
@@ -52,17 +99,31 @@ const main = async () =>  {
   console.log(`Hi ${player2[0]}! Your symbol is O`);
 
   let gameInProgress = true;
+  let numMoves = 0;
   while (gameInProgress) {
-    console.log(`${players[currPlayer][0]}, please enter your move.`)
-    let row = await placeMove(players[currPlayer][0], 'row');
-    let col = await placeMove(players[currPlayer][0], 'col');
-    gameDecisions[row][col] = players[currPlayer][1];
+    while(true) {
+      console.log(`${players[currPlayer][0]}, please enter your move.`)
+      let row = await placeMove(players[currPlayer][0], 'row');
+      let col = await placeMove(players[currPlayer][0], 'col');
+      
+      if(validMove(row, col)) {
+        numMoves++;
+        gameDecisions[row][col] = players[currPlayer][1];
+        break;
+      }
+      console.log('that is an invalid move.');
+    }
 
     board = setBoard(gameDecisions);
     console.log(board);
 
-    gameInProgress = checkWinConditions();
-    if(!gameInProgress) {
+    gameInProgress = checkWinConditions(players[currPlayer][1]);
+    if(!gameInProgress || numMoves === 9) {
+      if(numMoves === 9) {
+        console.log('The game ended in a tie!');
+      } else {
+        console.log(`${players[currPlayer][0]} has won the game!`);
+      }
       break;
     }
 
